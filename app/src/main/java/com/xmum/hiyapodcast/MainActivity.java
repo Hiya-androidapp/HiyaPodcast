71aae80a5625dfb1e6ce473f3da366b2a53902cf
aReddddd <920075317@qq.com>
2020-05-20 23:53:40 +0800

    v0.006

--- app/src/main/java/com/xmum/hiyapodcast/MainActivity.java
+++ app/src/main/java/com/xmum/hiyapodcast/MainActivity.java
@@ -1,6 +1,7 @@
 package com.xmum.hiyapodcast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -9,20 +10,27 @@ import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
 import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
 import com.ximalaya.ting.android.opensdk.model.category.Category;
 import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
-
+import com.xmum.hiyapodcast.adapters.IndicatorAdapter;
+import com.xmum.hiyapodcast.utils.LogUtil;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+//import android.support.v7.app.AppCompatActivity;
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
+    private  MagicIndicator mMagicIndicator;
+    private ViewPager mContentPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
         Map<String, String> map = new HashMap<>();
         CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
             @Override
@@ -32,17 +40,36 @@ public class MainActivity extends AppCompatActivity {
                     int size = categories.size();
                     Log.d(TAG, "categories size --- < " + size);
                     for (Category category : categories) {
-                        Log.d(TAG, "category --- >" + category.getCategoryName());
+                       // Log.d(TAG, "category --- >" + category.getCategoryName());
+                        LogUtil.d(TAG,"Category --> "+ category.getCategoryName());
                     }
                 }
             }
 
             @Override
             public void onError(int i, String s) {
-                Log.e(TAG, "error code -- " + i + "error msg == > " + s);
+                //Log.e(TAG, "error code -- " + i + "error msg == > " + s);
+                LogUtil.e(TAG, "error code -- " + i + "error msg == > " + s);
             }
         });
+*/
+        initView();
+    }
 
+    private void initView()
+    {
+        mMagicIndicator = this.findViewById(R.id.main_indicator);
+        mMagicIndicator.setBackgroundColor(this.getColor(R.color.main_color));
+        //create indicator adapter
+        IndicatorAdapter adapter = new IndicatorAdapter(this);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdapter(adapter);
 
+        //viewpager
+        mContentPager= this.findViewById(R.id.content_paper);
+
+        //bind viewpage and indicator together
+        mMagicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(mMagicIndicator, mContentPager);
     }
 }
