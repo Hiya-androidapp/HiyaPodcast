53265256bf581565a9b1bbea9287908fc3372f57
e <SWE1709247@xmu.edu.my>
2020-05-21 19:47:32 +0800

    第十个视频结束

--- app/src/main/java/com/xmum/hiyapodcast/utils/LogUtil.java
+++ app/src/main/java/com/xmum/hiyapodcast/utils/LogUtil.java
@@ -2,14 +2,24 @@ package com.xmum.hiyapodcast.utils;
 
 import android.util.Log;
 
+
 public class LogUtil {
 
     public static String sTAG = "LogUtil";
 
-
+    //控制是否要输出log
     public static boolean sIsRelease = false;
 
+    /**
+     * 如果是要发布了，可以在application里面把这里release一下，这样子就没有log输出了
+=======
+    //control whether output log
+    public static boolean sIsRelease = false;
 
+    /**
+     * when it will be released，can release in application first，so that no log output.
+>>>>>>> b0a17fa9cf6a7ddc651a2a7f3c8d772f76574931
+     */
     public static void init(String baseTag, boolean isRelease) {
         sTAG = baseTag;
         sIsRelease = isRelease;
