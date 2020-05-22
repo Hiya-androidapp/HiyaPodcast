a022e5ac90129e7869fbf196d1a77cb3999aa75a
KokuoU <swe1709127@xmu.edu.my>
2020-05-22 12:56:24 +0800

    视频11完成

--- app/src/main/java/com/xmum/hiyapodcast/fragments/RecommendFragment.java
+++ app/src/main/java/com/xmum/hiyapodcast/fragments/RecommendFragment.java
@@ -5,12 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
 import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
 import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
 import com.ximalaya.ting.android.opensdk.model.album.Album;
 import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
 import com.xmum.hiyapodcast.R;
+import com.xmum.hiyapodcast.adapters.RecommendListAdapter;
 import com.xmum.hiyapodcast.base.BaseFragment;
 import com.xmum.hiyapodcast.utils.Constants;
 import com.xmum.hiyapodcast.utils.LogUtil;
@@ -22,13 +26,27 @@ import java.util.Map;
 public class RecommendFragment extends BaseFragment {
 
     private static final String TAG = "RecommendFragment";
+    private View mRootView;
+    private RecyclerView mRecommendRv;
+    private RecommendListAdapter mRecommendListAdapter;
 
     @Override
     protected View onSubViewLoaded(LayoutInflater layoutInflater, ViewGroup container) {
-        View rootView = layoutInflater.inflate(R.layout.fragment_recommend, container, false);
+        mRootView = layoutInflater.inflate(R.layout.fragment_recommend, container, false);
 
+        //the use of RecyclerView
+        //1. find the controller
+        mRecommendRv = mRootView.findViewById(R.id.recommend_list);
+        //2. set layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecommendRv.setLayoutManager(linearLayoutManager);
+        //3. set adapter
+        mRecommendListAdapter = new RecommendListAdapter();
+        mRecommendRv.setAdapter(mRecommendListAdapter);
+        //fetch data
         getRecommendData();
-        return rootView;
+        return mRootView;
     }
 //get recommend resource(guess what u like)
     private void getRecommendData() {
@@ -37,21 +55,25 @@ public class RecommendFragment extends BaseFragment {
         CommonRequest.getGuessLikeAlbum(map, new IDataCallBack<GussLikeAlbumList>() {
             @Override
             public void onSuccess(GussLikeAlbumList gussLikeAlbumList) {
-                if(gussLikeAlbumList!=null){
+                if(gussLikeAlbumList != null){
                     List<Album> albumList=gussLikeAlbumList.getAlbumList();
-                    if(albumList!=null) {
-                        LogUtil.d(TAG, "size -->" + albumList.size());
-                    }
+                    //after getting data, update UI
+                    upRecommendUI(albumList);
                 }
             }
 
             @Override
             public void onError(int i, String s) {
-                LogUtil.d(TAG,"error -->"+i);
-                LogUtil.d(TAG,"errorMsg -->"+s);
+                LogUtil.d(TAG,"error -- >" + i);
+                LogUtil.d(TAG,"errorMsg -- >" + s);
             }
         });
 
 
+    }
+
+    private void upRecommendUI(List<Album> albumList) {
+        //set data to adapter, and update UI
+        mRecommendListAdapter.setData(albumList);
     }
 }
