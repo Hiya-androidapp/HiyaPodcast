a7cb9b1a4a84a2b7b57e9ae5fad21ea2db4560d1
aReddddd <920075317@qq.com>
2020-05-28 20:48:34 +0800

    init

---
+++ app/src/main/java/com/xmum/hiyapodcast/utils/LogUtil.java
@@ -0,0 +1,47 @@
+package com.xmum.hiyapodcast.utils;
+
+import android.util.Log;
+
+public class LogUtil {
+
+    public static String sTAG = "LogUtil";
+
+
+    public static boolean sIsRelease = false;
+
+
+    public static void init(String baseTag, boolean isRelease) {
+        sTAG = baseTag;
+        sIsRelease = isRelease;
+    }
+
+    public static void d(String TAG, String content) {
+        if (!sIsRelease) {
+            Log.d("[" + sTAG + "]" + TAG, content);
+        }
+    }
+
+    public static void v(String TAG, String content) {
+        if (!sIsRelease) {
+            Log.d("[" + sTAG + "]" + TAG, content);
+        }
+    }
+
+    public static void i(String TAG, String content) {
+        if (!sIsRelease) {
+            Log.d("[" + sTAG + "]" + TAG, content);
+        }
+    }
+
+    public static void w(String TAG, String content) {
+        if (!sIsRelease) {
+            Log.d("[" + sTAG + "]" + TAG, content);
+        }
+    }
+
+    public static void e(String TAG, String content) {
+        if (!sIsRelease) {
+            Log.d("[" + sTAG + "]" + TAG, content);
+        }
+    }
+}
